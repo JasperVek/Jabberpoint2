6a2ec8099821faf199ea8d6eaf1955bbcd63e632
JasperVek <jasper.vek@gmail.com>
2019-01-11 12:57:36 +0100

    1

---
+++ src/MouseController.java
@@ -0,0 +1,59 @@
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputListener;
+
+
+public class MouseController implements MouseInputListener 
+{
+	
+	/*
+	 TODO 
+	 */
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// TODO gaat over naar mouseMoved, dus doet verder niks?
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// TODO activeren????
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// TODO de menu knop die de boel deactiveert???
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	// TODO het tekenen zelf want het blijft ingedrukt?
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
